398fa698eeacba2e3703ee7fc68cd53c36cdbcb5
ShawnGHB <shawnshawn4116@gmail.com>
2024-09-24 09:16:14 -0400

    Added Demo
    In class Demo Added

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int outTemp = 34;
+            int inTemp = 26;
+
+            Console.WriteLine("It's " + (outTemp - inTemp) + " Degrees Less In Here!");
+        }
+    }
+}
